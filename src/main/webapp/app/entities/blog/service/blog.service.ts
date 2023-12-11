7c71331b19503cb731bc87a4d0683470a967ddf9
Sebastian Garcia Ojeda <jsgarciaojeda@gmail.com>
2023-12-11 07:11:51 +0000

    removed debug string

--- src/main/webapp/app/entities/blog/service/blog.service.ts
+++ src/main/webapp/app/entities/blog/service/blog.service.ts
@@ -42,16 +42,10 @@ export class BlogService {
     // Nombre de la caché que deberías haber utilizado para almacenar estos datos
     const cacheName = 'miCache';
     
-    const exampleData = {
-	    id: 1,
-	    title: 'Ejemplo de Blog',
-	    content: 'Este es un blog de ejemplo',
-    };
-
     if (blog) {
       // Abre la caché y almacena el nuevo blog solo si no es nulo
       caches.open(cacheName).then((cache) => {
-        const response = new Response(JSON.stringify(exampleData));
+        const response = new Response(JSON.stringify(blog));
         cache.put('/api/blogs/', response);
       });
     }
